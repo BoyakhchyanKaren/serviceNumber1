4c27a38dc94a34e7328b38ca8f197f9c2635c60c
Karen Boyakhchyan <karenboyakhchyan20@gmail.com>
2022-01-05 18:30:19 +0400

    added google part

--- src/app.ts
+++ src/app.ts
@@ -4,7 +4,7 @@ import helmet from 'helmet';
 import morgan from 'morgan';
 import compression from 'compression';
 import SwaggerUI from 'swagger-ui-express';
-import * as SwaggerDoc from "./swagger/openApi.json";
+import * as SwaggerDoc from './swagger/openApi.json';
 import { serviceRoutes } from './routes/serviceRoutes';
 import { commentRoutes } from './routes/commentRoutes';
 import { questionRoutes } from './routes/questionRoutes';
@@ -12,13 +12,13 @@ import { HttpErr } from './exceptions/HttpError';
 import { errorHandler } from './controllers/errorHandler';
 import registerRoutes from './routes/registerRoutes';
 import ruleRoutes from './routes/ruleRoutes';
-import { authMiddleware } from './middleware/auth-middleware';
+import googleRoutes from './routes/googleRoutes';
 
 export const getApplication = (): Express => {
   const app = express()
     .use(express.json())
     .use(cors({
-      origin:"*",
+      origin: '*'
     }))
     .use(helmet())
     .use(morgan('dev'))
@@ -28,9 +28,10 @@ export const getApplication = (): Express => {
     .use('/api/v1/questions', questionRoutes)
     .use('/api/v1', registerRoutes)
     .use('/api/v1/rules', ruleRoutes)
+    .use('/api/v1', googleRoutes)
     .use('/api/v1/Swagger', SwaggerUI.serve, SwaggerUI.setup(SwaggerDoc))
     .get('/', (req: Request, res: Response) => {
-      res.redirect("https://pink-team-deployment.herokuapp.com/api/v1/Swagger")
+      res.redirect('https://pink-team-deployment.herokuapp.com/api/v1/Swagger');
     })
     .all('*', (req: Request, res: Response, next: NextFunction) => {
       next(HttpErr.notFound(`Can't find ${req.originalUrl} on this server!`));
---
+++ src/middleware/checkAuthenticated.ts
@@ -0,0 +1,40 @@
+import { NextFunction, Request } from 'express';
+import { Response } from 'express/ts4.0';
+import { OAuth2Client } from 'google-auth-library';
+import { HttpErr } from '../exceptions/HttpError';
+import ExceptionMessages from '../exceptions/messages';
+const client = new OAuth2Client(String(process.env.CLIENT_ID));
+
+interface userI {
+  given_name:string,
+  family_name:string,
+  email:string,
+  picture:string,
+};
+
+export function checkAuthenticated(req:Request, res:Response, next:NextFunction){
+  // const token = req.cookies["session-token"];
+  const { token } = req.body;
+  let user:userI;
+
+  async function verify(){
+    const ticket = await client.verifyIdToken({
+      idToken:token,
+      audience:String(process.env.CLIENT_ID)
+    });
+    const payload = await ticket.getPayload();
+    user.given_name = <string>payload?.given_name;
+    user.family_name = <string>payload?.family_name;
+    user.email = <string>payload?.email;
+    user.picture = <string>payload?.picture;
+  }
+  verify()
+    .then(() => {
+      //@ts-ignore
+      req.loggedUser = user;
+      next();
+    })
+    .catch(err=> {
+      next(HttpErr.badRequest(ExceptionMessages.INTERNAL))
+    })
+}
